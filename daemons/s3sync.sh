4ebe958d11f3df895d6c8c81ada8bac8d7a3b83d
Lee Edwards <leeredwards@gmail.com>
2022-11-30 15:23:32 -0800

    Use absolute path for home in daemon

--- daemons/s3sync.sh
+++ daemons/s3sync.sh
@@ -2,7 +2,7 @@
  
 while true
 do
-    aws s3 sync s3://rootvc-dreambooth/input ~/rootvc/dreambooth/s3/input
-    aws s3 sync s3://rootvc-dreambooth/output ~/rootvc/dreambooth/s3/output
+    aws s3 sync s3://rootvc-dreambooth/input /home/ec2-user/rootvc/dreambooth/s3/input
+    aws s3 sync /home/ec2-user/rootvc/dreambooth/s3/output s3://rootvc-dreambooth/output
     sleep 10
 done
