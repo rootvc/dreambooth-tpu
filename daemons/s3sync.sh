f413c6c24befb7d74fd8e7ee0aef1074ebab89f5
Lee Edwards <leeredwards@gmail.com>
2022-11-30 15:59:05 -0800

    Expand home dir in daemon

--- daemons/s3sync.sh
+++ daemons/s3sync.sh
@@ -2,7 +2,7 @@
  
 while true
 do
-    sudo -u ec2-user aws s3 sync s3://rootvc-dreambooth/input ~/rootvc/dreambooth/s3/input
-    sudo -u ec2-user aws s3 sync ~/rootvc/dreambooth/s3/output s3://rootvc-dreambooth/output
+    sudo -u ec2-user aws s3 sync s3://rootvc-dreambooth/input /home/ec2-user/rootvc/dreambooths3/input
+    sudo -u ec2-user aws s3 sync /home/ec2-user/rootvc/dreambooth/s3/output s3://rootvc-dreambooth/output
     sleep 10
 done
