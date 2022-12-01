8293043ab3a0e75886efce047f17857fd2d3cd1f
Lee Edwards <leeredwards@gmail.com>
2022-12-01 15:57:35 -0500

    Log end of processing

--- daemons/dreamwatcher.sh
+++ daemons/dreamwatcher.sh
@@ -6,5 +6,6 @@ while true
 do
     # Execute python script that checks for new inputs and processing
     conda run -n db --no-capture-output python $DREAMBOOTH_DIR/daemons/src/process.py
+    echo Finished processing last input
     sleep 10
 done
