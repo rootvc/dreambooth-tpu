b7fc0c0147f43906bce37e2d838a58b693170359
Lee Edwards <leeredwards@gmail.com>
2022-12-01 00:54:59 -0500

    oops, got s3 sync cli backward

--- daemons/s3sync.sh
+++ daemons/s3sync.sh
@@ -4,7 +4,8 @@ set -x
 while true
 do
     sudo -u ec2-user aws s3 sync s3://rootvc-dreambooth/input /home/ec2-user/rootvc/dreambooth/s3/input
-    sudo -u ec2-user aws s3 sync /home/ec2-user/rootvc/dreambooth/s3/output s3://rootvc-dreambooth/output
+    sudo -u ec2-user aws s3 sync s3://rootvc-dreambooth/output /home/ec2-user/rootvc/dreambooth/s3/output 
+
     sleep 10
 done
 
