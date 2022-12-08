771fadd3d90ca170e4fc840232ef9ec04b4e50b1
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-07 18:21:04 -0800

    try flax

--- train.sh
+++ train.sh
@@ -17,7 +17,7 @@ cp ./s3/photobooth-input/$2*.jpg ./input/$1
 
 conda run -n db --no-capture-output \
   accelerate launch --num_cpu_threads_per_process=96 \
-  diffusers/examples/dreambooth/train_dreambooth.py \
+  diffusers/examples/dreambooth/train_dreambooth_flax.py \
   --pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5" \
   --instance_data_dir="./input/$1" \
   --class_data_dir="./s3/class/" \
