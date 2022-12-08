8530e6798ef4eceb9373c2271974fb0d1a69f79f
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-08 03:14:56 -0800

    go all in

--- train.sh
+++ train.sh
@@ -19,7 +19,7 @@ cp ./s3/photobooth-input/$2*.jpg ./input/$1
 conda run -n db --no-capture-output \
   accelerate launch --num_cpu_threads_per_process=96 \
   diffusers/examples/dreambooth/train_dreambooth_flax.py \
-  --pretrained_model_name_or_path="duongna/stable-diffusion-v1-4-flax" \
+  --pretrained_model_name_or_path="flax/stable-diffusion-2-1" \
   --instance_data_dir="./input/$1" \
   --class_data_dir="./s3/class/" \
   --output_dir="./models/" \
@@ -27,6 +27,7 @@ conda run -n db --no-capture-output \
   --instance_prompt="a photo of sks person" \
   --class_prompt="a photo of person" \
   --train_batch_size=1 \
+  --train_text_encoder \
   --learning_rate=5e-6 \
   --lr_scheduler="constant" \
   --num_class_images=300 \
