ba916f15b0de263888dcd574a90dd3423b27e8bc
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-08 19:16:28 -0800

    optimize for inference when generating

--- generate.sh
+++ generate.sh
@@ -14,7 +14,7 @@ echo Transfer learning beginning at step: $RETRAIN_STEP
 
 mkdir -p ./s3/output/$1
 
-accelerate launch --num_cpu_threads_per_process=96 \
+accelerate launch --num_cpu_threads_per_process=96 --dynamo_backend=ofi \
     src/inference.py \
     --model_dir="./models" \
     --output_dir="./s3/output" \
