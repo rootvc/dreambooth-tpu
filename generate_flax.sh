02b514b5677e6e9affcc79f29b553163ed490ac9
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-20 05:19:38 -0800

    fix paths

--- generate_flax.sh
+++ generate_flax.sh
@@ -39,5 +39,5 @@ numactl --cpunodebind=0 \
     --output_path ../s3/output/$1
 popd
 
-mv ../s3/output/$1/final_results/*.jpg ../s3/output/$1/
-rm -r ../s3/output/$1/{restored_faces,final_results,cropped_faces}
+mv ./s3/output/$1/final_results/*.jpg ./s3/output/$1/
+rm -r ./s3/output/$1/{restored_faces,final_results,cropped_faces}
