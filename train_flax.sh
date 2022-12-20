7fd4511c5e63109bad2ab56a058e1a5e546dbd82
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-20 02:02:50 -0800

    Add VAE

--- train_flax.sh
+++ train_flax.sh
@@ -20,8 +20,9 @@ numactl --cpunodebind=0 \
   accelerate launch --num_cpu_threads_per_process=96 \
   diffusers/examples/dreambooth/train_dreambooth_flax.py \
   --pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5" \
-  --revision "flax" \
-  --resolution 256 \
+  --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
+  --revision="flax" \
+  --resolution=256 \
   --instance_data_dir="./input/$1" \
   --class_data_dir="./s3/class/" \
   --output_dir="./models/" \
