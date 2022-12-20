38ecb97102a5bcebe4f966366f677dff3c3dd929
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-19 23:11:36 -0800

    try less complex training

--- train_flax.sh
+++ train_flax.sh
@@ -21,19 +21,21 @@ numactl --cpunodebind=0 \
   diffusers/examples/dreambooth/train_dreambooth_flax.py \
   --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1" \
   --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
-  --cache_latents \
+  \
   --revision="bf16" \
   --instance_data_dir="./input/$1" \
   --class_data_dir="./s3/class/" \
   --output_dir="./models/" \
-  --with_prior_preservation --prior_loss_weight=1.0 \
+  \
   --instance_prompt="a photo of sks person" \
   --class_prompt="a photo of person" \
   --train_batch_size=4 \
   --learning_rate=5e2 \
   --train_text_encoder \
-  --augment_images \
+  \
   --num_class_images=300 \
   --max_train_steps=$STEPS \
   --mixed_precision=bf16 \
-  --save_steps=$INTERVAL
+  --save_steps=$INTERVAL # --cache_latents \
+# --with_prior_preservation --prior_loss_weight=1.0 \
+# --augment_images \
