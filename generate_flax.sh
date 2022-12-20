edb2bdb8f3804d5ff94635a0604857f4aa20ccf8
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-20 04:35:15 -0800

    moar guidance scale

--- generate_flax.sh
+++ generate_flax.sh
@@ -24,19 +24,19 @@ numactl --cpunodebind=0 \
     --id $1 \
     --num-images 4 \
     --step $RETRAIN_STEP \
-    --prompt "scene from an animated tv show, disney short, colorful" \
-    --prompt "anime character, naruto, scene from an anime cartoon" \
-    --prompt "ninja, thief, cyberpunk, synthwave, retro, bold, sneaky pose" \
+    --prompt "cartoon tv show, disney animated short, colorful" \
+    --prompt "anime character, naruto, scene from an anime cartoon, japanese anime, shonen jump" \
+    --prompt "an assassin sneaking through a dilapidated alley, movie still, 30mm" \
+    --prompt "a middle-eastern monarch, a ruler surrounded by riches and servants, with glowing sands in the background" \
     --prompt "winter gothic, leather, gothic jewellery, flowing cloak, elegant pose" \
     --prompt "impressionist painting, Daniel F Gerhartz, nature" \
     --prompt "pencil sketch, 4 k, 8 k, absolute detail, black and white drawing" \
-    --prompt "Film still from Avatar, cinematography by James Cameron, 2020, dramatic lighting, bokeh" \
     --prompt "detailed ink drawing, Lone Wolf and Cub manga panel 4 k, full body, sword slash, manga" \
     --prompt "hallucination from 1970, hippie, tripping, acid, rainbow, daydream" \
     --prompt "cinematic still, person with glasses as rugged warrior, threatening xenomorph, alien movie" \
-    --prompt "colorful cinematic still of sksxvs2 man with glasses, armor, cyberpunk, with a xenonorph, in alien movie (1986),background made of brain cells, organic, ultrarealistic, leic 30mm" \
-    --prompt "teampunk warrior, neon organic vines, glasses, digital painting" \
-    --prompt "soldier in world war one, dreary, depressing, grey, raining, gloomy, war, death" \
+    --prompt "colorful cinematic still with glasses, armor, cyberpunk, with a xenonorph, in alien movie (1986),background made of brain cells, organic, ultrarealistic, leic 30mm" \
+    --prompt "steampunk warrior, neon organic vines, glasses, digital painting" \
+    --prompt "dying soldier in the trenches of world war one, army uniform, depressing, grey, morose" \
     --prompt "person in advanced organic armor, biological filigree, flowing hair, neon details, intricate, elegant, highly detailed, digital painting, artstation, concept art, smooth, sharp focus, octane, art by Krenz Cushart , Artem Demura, Alphonse Mucha, digital cgi art 8K HDR by Yuanyuan Wang photorealistic" \
     --prompt "Retro comic style artwork, highly detailed James Bond, comic book cover, symmetrical, vibrant"
 
--- src/inference_flax.py
+++ src/inference_flax.py
@@ -124,7 +124,7 @@ def main():
             jit=True,
             prng_seed=prng_seed,
             num_inference_steps=100,
-            guidance_scale=12.0,
+            guidance_scale=14.0,
         ).images
         pils = pipe.numpy_to_pil(
             np.asarray(images.reshape((device_count,) + images.shape[-3:]))
